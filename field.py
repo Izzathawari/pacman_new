92ee6d1b6402d73d44405297b6cf42226af5eca2
izzathawari <izzatwari@gmail.com>
2024-10-07 22:10:57 +0900

    Added field class description

---
+++ field.py
@@ -0,0 +1,14 @@
+
+class field :
+    """ 
+    fieldクラス
+
+    fieldクラスはpacmanのplayerとenemyの動き空間である。またfoodとblockもここに表示する。
+
+    Attribute: 
+    block: 障害の数と位置を設定する。
+    food: 食べ物の数と位置を設定する。
+    player: playerの初度の位置
+    enemy: enemyの初度の位置
+
+    """
