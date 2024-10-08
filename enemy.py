4abea13f51e9976f5e852bfd030be4cfc05303d9
luqmanhkm1211 <luqmanhakim.lhha@gmail.com>
2024-10-08 16:53:26 +0900

    Add enenmy.py

--- enemy.py
+++ enemy.py
@@ -1,16 +1,24 @@
 class Enemy:
-    """
+    """エネミークラス
     敵キャラクターを表すクラス。
     アイテムの基礎機能を継承し、敵キャラクターのランダムな移動を管理する。
 
     Attributes:
-        icon (str): 敵を表すアイコン
-        now_x (int): 現在のx座標
-        now_y (int): 現在のy座標
-        next_x (int): 次の時刻でのx座標
-        next_y (int): 次の時刻でのy座標
-        status (bool): 敵の状態 (True: 存在する, False: 消滅した)
+　　　　　self.icon(str) : 表示されるアイテムのアイコン
+        self.now_x(int) : 現在のx座標
+        self.now_y(int) : 現在のy座標
+        self.next_x(int) : 次の時刻でのx座標
+        self.next_y(int) : 次の時刻でのy座標
+        self.status(bool) : アイテムの状態（Trueなら存在する、Falseなら存在しない消滅した）
     """
+
+
+
+
+
+
+
+
 
     def __init__(self, x: int, y: int) -> None:
         """
