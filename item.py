9933e4f57b4bb17e1d2766f26794d00279617e3b
luqmanhkm1211 <luqmanhakim.lhha@gmail.com>
2024-10-15 17:48:51 +0900

    Updated item.py

--- item.py
+++ item.py
@@ -2,16 +2,15 @@
 block,enemy,food,playerの親クラス
 """
 
+
 class Item:
     """block,enemy,food,playerの親クラス。
 
-    ゲーム内のアイテムを表すクラス。
-    
     Attributes:
         now_x (int): 現在のx座標
         now_y (int): 現在のy座標
-        next_x (int): 次に移動する予定のx座標
-        next_y (int): 次に移動する予定のy座標
+        next_x (int): 次の時刻でのx座標
+        next_y (int): 次の時刻でのy座標
         status (bool):  アイテムの状態（Trueなら存在する、Falseなら存在しない消滅した）
         icon(str) : 表示されるアイテムのアイコン
     """
@@ -19,12 +18,12 @@ class Item:
     def __init__(self, x, y) -> None:
         """
         Item初期座標と初期状態を設定する。
-  
+
         Args:
             x (int): 初期のx座標
             y (int): 初期のy座標
-    
-        Returns: 
+
+        Returns:
              None
 
         Examples:
@@ -91,7 +90,8 @@ class Item:
             (3, 4)
 
         """
-    
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
